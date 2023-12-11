11e9ce7e030b0ad726cbe00095ad49d969bdbec1
Kyedonghwan <kdh6437@nts-corp.com>
2023-12-12 00:40:59 +0900

    connection with db

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import cors from 'cors';
 
+import todos from "./models/todo";
+import todo from './models/todo';
+
+dotenv.config({ path: path.resolve(__dirname, './.env') });
+
 const app = express();
-dotenv.config();
 
 const  { PORT, MONGO_URI } = process.env;
 
@@ -17,7 +22,7 @@ app.use(bodyParser.json());
 
 //mongoose setup
 mongoose.Promise = global.Promise;
-mongoose.connect( MONGO_URI!);
+mongoose.connect("mongodb://127.0.0.1:27017/todos");
 
 let db = mongoose.connection;
 
@@ -26,18 +31,26 @@ db.once('open', () => {
   console.log("connected to mongod server");
 })
 
-mongoose.connect('mongodb://localhost/todos');
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+app.listen(3001, () => {
+  console.log(`Example app listening on port 3001`)
 })
 
 app.get("/api/get", (req, res) => {
   res.send("donghwan");
 });
 
-app.post("/api/post", (req,res) => {
+app.post("/api/post", async (req,res) => {
   const content = req.body.content;
-  res.send(content);
-  console.log(req);
+  const newTodos = new todos({
+    content: content
+  });
+
+  try {
+    await newTodos.save();
+    console.log(todos.find());
+    console.log(newTodos);
+    console.log("complete");
+  } catch(e) {
+    console.log(e);
+  }
 })
--- server/models/todo.ts
+++ server/models/todo.ts
@@ -4,8 +4,4 @@ const todoSchema = new mongoose.Schema({
   content: { type: String }
 });
 
-todoSchema.statics.findAll = () => {
-  return this.find({});
-}
-
 export default mongoose.model('todos', todoSchema);
